a5d552a9d78d0f0062fbb1789f2e18d987d628b9
kuba <kuba@kuba-H81M-S1>
2022-10-20 00:58:25 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

---
+++ src/main/java/pwo/app/Sequence.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pwo.app;
+
+/**
+ *
+ * @author kuba
+ */
+public class Sequence {
+
+    public static void main(String[] args) {
+
+        (new SeqToFileApp()).run(args);
+    }
+}
